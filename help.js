e9e0c1218fc40d76ee7a0fbf5cfa58abbc25d3e2
Darko Drakulic <ddrakulic@gmail.com>
2016-08-25 21:11:02 +0200

    Initial upload

---
+++ help.js
@@ -0,0 +1,42 @@
+var JSXGraph_GUI_help = new Array();
+
+JSXGraph_GUI_help["slo"] = new Array();
+
+JSXGraph_GUI_help["slo"]["_ARROW_"] = "Izberi orodje.";
+JSXGraph_GUI_help["slo"]["_ERASE_"] = "<strong>Brisanje objekta</strong>: izberi objekt, ki ga želiš odstraniti."; 
+JSXGraph_GUI_help["slo"]["_PENCIL_"] = "<strong>Pisalo</strong>: prostoročno risanje.";
+JSXGraph_GUI_help["slo"]["_POINT_"] = "<strong>Točka</strong>: izberi koordinate točke."; 
+JSXGraph_GUI_help["slo"]["_SEGMENT_"] = "<strong>Daljica</strong>: izberi krajišči daljice.";
+JSXGraph_GUI_help["slo"]["_FIXED_SEGMENT_"] = "<strong>Daljica z dano dolžino </strong>: izberi začetno točko in vnesi dolžino daljice.";
+JSXGraph_GUI_help["slo"]["_LINE_"] = "<strong>Premica</strong>: izberi dve točki, ki določata premico.";
+JSXGraph_GUI_help["slo"]["_HLINE_"] = "<strong>Poltrak</strong>: izberi začetno in dodatno točko, ki določata poltrak."; 
+JSXGraph_GUI_help["slo"]["_CIRCLE_"] = "<strong>Krožnica</strong>: izberi središče in točko na krožnici."; 
+JSXGraph_GUI_help["slo"]["_CIRCLE3_"] = "<strong>Krožnica skozi tri točke</strong>: izberi tri točke, ki določajo krožnico.";
+JSXGraph_GUI_help["slo"]["_CONIC_SECTION5_"] = "<strong>Stožnica skozi pet točk</strong>: izberi pet točk, ki določajo stožnico.";
+JSXGraph_GUI_help["slo"]["_CIRCLER_"] = "<strong>Krožnica s podanim polmerom</strong>: izberi središče krožnice in vnesi polmer.";
+JSXGraph_GUI_help["slo"]["_POLYGON_"] = "<strong>Mnogokotnik</strong>: izberi oglišča mnogokotnika (zadnje mora biti enako začetnemu).";
+JSXGraph_GUI_help["slo"]["_REGULAR_POLYGON_"] = "<strong>Pravilni mnogokotnik</strong>: izberi dve točki in število oglišč.";
+JSXGraph_GUI_help["slo"]["_ANGLE_"] = "<strong>Kot</strong>: izberi tri točke, ki določajo kot.";
+JSXGraph_GUI_help["slo"]["_ANGLEV_"] = "<strong>Kot z izpisom velikosti</strong>: izberi tri točke, ki določajo kot.";
+JSXGraph_GUI_help["slo"]["_ANGLES_"] = "<strong>Kot</strong>: izberi tri točke, ki določajo kot.";
+JSXGraph_GUI_help["slo"]["_INTERSECTION_"] = "<strong>Presek</strong>: izberi objekte, katerih presek iščeš.";
+JSXGraph_GUI_help["slo"]["_ANGLE_BISECTOR_"] = "<strong>Simetrala kota</strong>: izberi tri točke, ki določajo kot.";
+JSXGraph_GUI_help["slo"]["_MIDPOINT_"] = "<strong>Središče daljice</strong>: izberi krajišči daljice.";
+JSXGraph_GUI_help["slo"]["_PERPENDICULAR_"] = "<strong>Pravokotnica</strong>: izberi daljico/premico/poltrak in točko.";
+JSXGraph_GUI_help["slo"]["_PARALLEL_"] = "<strong>Vzporednica</strong>: izberi daljico/premico/poltrak in točko.";
+JSXGraph_GUI_help["slo"]["_SYMMETRY_"] = "<strong>Središčna simetrija</strong>: izberi točko, ki jo želiš preslikati, in središče simetrije.";
+JSXGraph_GUI_help["slo"]["_SEGMENT_SYMMETRY_"] = "<strong>Simetrala daljice</strong>: izberi daljico.";
+JSXGraph_GUI_help["slo"]["_LINE_SYMMETRY_"] = "<strong>Osna simetrija</strong>: izberi točko in os simetrije.";
+JSXGraph_GUI_help["slo"]["_DISTANCE_"] = "<strong>Razdalja</strong>: izberi dve točki.";
+JSXGraph_GUI_help["slo"]["_AREA_"] = "<strong>Ploščina</strong>: izberi mnogokotnik.";
+JSXGraph_GUI_help["slo"]["_VECTOR_"] = "<strong>Vektor</strong>: izberi začetno in končno točko.";
+JSXGraph_GUI_help["slo"]["_TRANSLATION_"] = "<strong>Vzporedni premik za vektor</strong>: izberi točko in vektor.";
+JSXGraph_GUI_help["slo"]["_TEXT_"] = "";
+JSXGraph_GUI_help["slo"]["_EMPTY_TEXT_"] = "Vnesite tekst";
+
+JSXGraph_GUI_help["slo"]["_CIRCLER_POPUP_"] = "Vnesi polmer krožnice";
+JSXGraph_GUI_help["slo"]["_ANGLEV_POPUP_"] = "Vnesi velikost kota";
+JSXGraph_GUI_help["slo"]["_FIXED_SEGMENT_POPUP_"] = "Dolžina";
+JSXGraph_GUI_help["slo"]["_CONSOLE_POPUP_"] = "Napaka! Napačen vnos";
+JSXGraph_GUI_help["slo"]["_RPOLYGON_POPUP_"] = "Oglišča";
+
